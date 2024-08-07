80ac4ae4df501fe7f3acd90e12b34b9e78b021f2
LucasBorges-91 <lucas.pborges@yahoo.com.br>
2024-08-07 19:19:12 -0300

    fourth class - implementing mvvm

---
+++ MemoryGame.swift
@@ -0,0 +1,36 @@
+//
+//  MemorizeGame.swift
+//  memorizeapp
+//
+//  Created by LUCAS PORTAL on 06/08/24.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent ) {
+        cards = Array<Card>()
+        for pairIndex in 0..<max(2,numberOfPairsOfCards) {
+            let content: CardContent = cardContentFactory( pairIndex )
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card ) {
+        
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    struct Card {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        let content: CardContent
+    }
+    
+}
--- memorizeapp/EmojiMemoryGame.swift
+++ memorizeapp/EmojiMemoryGame.swift
@@ -7,7 +7,34 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻","🎃","👹","👺","😈","💀","☠️","👿","👾","🕷️","🕸️","🦇","🫎","😱","🫥","🍭","🍬"]
     
-    var model: MemoryGame<String>
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+            
+        }
+    }
+    
+
+    @Published private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
 }
