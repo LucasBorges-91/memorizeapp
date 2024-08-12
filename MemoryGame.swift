d01f3452a84003a931aa6806d7163d81f45fea0a
LucasBorges-91 <lucas.pborges@yahoo.com.br>
2024-08-12 15:52:23 -0300

    Fifth class - creation animation using protocol to suport.

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent ) {
         cards = Array<Card>()
         for pairIndex in 0..<max(2,numberOfPairsOfCards) {
             let content: CardContent = cardContentFactory( pairIndex )
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
         }
     }
     
@@ -27,10 +27,12 @@ struct MemoryGame<CardContent> {
         cards.shuffle()
     }
     
-    struct Card {
+    struct Card: Equatable, Identifiable {
         var isFaceUp: Bool = true
         var isMatched: Bool = false
         let content: CardContent
+        
+        var id: String
     }
     
 }
